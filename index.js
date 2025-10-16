05509b362978fecfbefeb14c5bbcd16c4b9840af
Ekaterina Akshonova <eakshonova@gmail.com>
2025-10-16 20:00:13 +0200

    add task2

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Conflicts")
+const root = document.querySelector("#root");
+const button = document.querySelector("backgroundbutton");
+
+const root = document.querySelector("#root");
+const title = document.querySelector("h1");
+const button2 = document.querySelector("#background-button2");
+const button = document.querySelector("#background-button");
+
+const changeBackground = ()=>{
+  root.style.background = "red";
+};
+
+const changeTitle = ()=>{
+  title.textContent = "Technologies: GIT";
+};
+
+button.addEventListener("click",changeBackground);
